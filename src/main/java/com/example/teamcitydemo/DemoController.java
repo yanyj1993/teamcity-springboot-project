5f87d1784b7e137922cf98bf6493ebd9aacdb810
yanyj <yanyj@zjugis.com>
2021-06-21 17:19:55 +0800

    demo

---
+++ src/main/java/com/example/teamcitydemo/DemoController.java
@@ -0,0 +1,13 @@
+package com.example.teamcitydemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    @GetMapping("test")
+    public String test() {
+        return "test";
+    }
+}
